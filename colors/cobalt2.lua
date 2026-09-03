8fd4d88d89cfdab25840806a5d470b2b5909f695
Daniel Arauz <daniel.arauz@fuga.com>
2026-09-03 15:26:09 +0200

    updated a few colors

--- colors/cobalt2.lua
+++ colors/cobalt2.lua
@@ -3,35 +3,37 @@
 -- Maintainer: Daniel Arauz
 -- License: MIT
 
-vim.cmd.highlight('clear')
-vim.o.background = 'dark'
-vim.g.colors_name = 'cobalt2'
+vim.cmd.highlight("clear")
+vim.o.background = "dark"
+vim.g.colors_name = "cobalt2"
 
-local palette = require('cobalt2.palette')
-local groups = require('cobalt2.highlights')
+local palette = require("cobalt2.palette")
+local groups = require("cobalt2.highlights")
 
 ---@param name string
 ---@param val vim.api.keyset.highlight
 local function hi(name, val)
-  val.force = true
-  val.cterm = val.cterm or {}
-  vim.api.nvim_set_hl(0, name, val)
+	val.force = true
+	val.cterm = val.cterm or {}
+	vim.api.nvim_set_hl(0, name, val)
 end
 
 for name, spec in pairs(groups.ui()) do
-  hi(name, spec)
+	hi(name, spec)
 end
 
 for name, spec in pairs(groups.syntax()) do
-  hi(name, spec)
+	hi(name, spec)
 end
 
 for name, spec in pairs(groups.diagnostics()) do
-  hi(name, spec)
+	hi(name, spec)
 end
 
 for name, target in pairs(groups.links()) do
-  hi(name, { link = target })
+	vim.api.nvim_echo({ { "Linking " .. name .. " to " .. target, "Normal" } }, false, {})
+	-- print(name, target)
+	-- vim.api.nvim_set_hl(0, name, { link = target })
 end
 
 vim.g.terminal_ansi_colors = palette.ansi
